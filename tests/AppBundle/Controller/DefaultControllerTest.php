beb1cbfd531af91c9d8abd7162b75f5a92cbadff
natinho68 <mandemons1@hotmail.com>
2018-01-22 11:22:09 +0100

    testing urls response, with or without right roles

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -17,15 +17,6 @@ class DefaultControllerTest extends WebTestCase
         $this->client = static::createClient();
     }
 
-    public function test_indexAction()
-    {
-        $this->logIn();
-        $crawler = $this->client->request('GET', '/');
-
-        $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
-        $this->assertSame(1, $crawler->filter('html:contains("Bienvenue sur Todo List")')->count());
-    }
-
     private function logIn()
     {
         $session = $this->client->getContainer()->get('session');
@@ -39,6 +30,77 @@ class DefaultControllerTest extends WebTestCase
 
         $cookie = new Cookie($session->getName(), $session->getId());
         $this->client->getCookieJar()->set($cookie);
+    }
+
+    public function test_indexAction()
+    {
+        $this->logIn();
+        $crawler = $this->client->request('GET', '/');
+
+        $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+        $this->assertSame(1, $crawler->filter('html:contains("Bienvenue sur Todo List")')->count());
+    }
+
+
+    /**
+     * @dataProvider getUrls
+     */
+    public function test_SecureUrls($url)
+    {
+
+        $this->client->request('GET', $url);
+        $response = $this->client->getResponse();
+        $this->assertSame(Response::HTTP_FOUND, $response->getStatusCode());
+        $this->assertSame(
+            'http://localhost/login',
+            $response->getTargetUrl(),
+            sprintf('The %s secure URL redirects to the login form.', $url)
+        );
+    }
+
+
+    /**
+     * @dataProvider getAdminUrls
+     */
+    public function test_NotAllowedRoles($url)
+    {
+        $session = $this->client->getContainer()->get('session');
+
+        // the firewall context defaults to the firewall name
+        $firewallContext = 'main';
+
+        $token = new UsernamePasswordToken('admin', null, $firewallContext, array('ROLE_USER'));
+        $session->set('_security_'.$firewallContext, serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $this->client->getCookieJar()->set($cookie);
+
+
+        $this->client->request('GET', $url);
+        $this->assertSame(
+            Response::HTTP_FORBIDDEN,
+            $this->client->getResponse()->getStatusCode(),
+            sprintf('The %s URL return correctly forbidden.', $url)
+        );
+
+    }
+
+    public function getUrls()
+    {
+        yield ['/'];
+        yield ['/tasks/create'];
+        yield ['/users/create'];
+        yield ['/tasks/1/edit'];
+        yield ['/users/1/edit'];
+
+    }
+
+    public function getAdminUrls()
+    {
+        yield ['/users/1/edit'];
+        yield ['/users/create'];
+        yield ['/users'];
     }
 
     public function tearDown()
