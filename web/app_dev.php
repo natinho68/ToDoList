7e57a62a51955d1e82f18e910e9d1cec8289e432
natinho68 <mandemons1@hotmail.com>
2018-02-15 13:36:30 +0100

    disable cache by default

--- web/app_dev.php
+++ web/app_dev.php
@@ -24,10 +24,10 @@ Debug::enable();
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
 
-$kernel = new AppCache($kernel);
+//$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-Request::enableHttpMethodParameterOverride();
+//Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
